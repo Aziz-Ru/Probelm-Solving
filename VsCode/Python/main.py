c0a271121364d7ea11b014f2bb9dbd3bf01413f7
silicon <siliconoxide4@gmail.com>
2023-08-22 01:29:25 +0600

    Vscode Folder added

---
+++ VsCode/Python/main.py
@@ -0,0 +1,22 @@
+# Hello = "Hello world i,m from Bangladesh"
+# print(Hello.upper())
+# print(Hello.lower())
+# print(Hello.title())
+# p="Abdul"
+# q="aziz"
+# print(q.rstrip())
+# print(q.lstrip())
+# a=q.removeprefix("az")
+# b=q.removesuffix("iz")
+# print(a)
+# print(b)
+# print(p+' '+q)
+
+#list
+mylist=[3,4,5,1,2,5]
+mylist.append(8)
+print(mylist)
+mylist.sort()
+print(mylist) 
+mylist.insert(0,7)
+print(mylist)
