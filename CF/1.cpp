1614f14859f77f66049a0feafaa019b41bd96fe7
aziz <siliconoxide4@gmail.com>
2025-03-30 18:58:03 +0600

    complete

--- CF/1.cpp
+++ CF/1.cpp
@@ -8,9 +8,32 @@ using namespace std;
 #define ff first 
 #define ss second
 
-void solve(){
-    
-}
+int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size(), sum = 0, k = 0;
+        vector<int> diff(n + 1);
+
+        for(int i=0;i<n;i++){
+            while(sum+diff[i]<nums[i]){
+                k++;
+                if(k>queries.size())return -1;
+                int left=queries[k-1][0],right=queries[k-1][1],val=queries[k-1][2];
+                
+                if(right>=i){
+                    diff[max(i,left)]+=val;
+                    diff[right+1]-=val;
+                }
+                
+
+            }
+            for(int i=0;i<=n;i++){
+                cout<<diff[i]<<' ';
+            }
+            cout<<'\n';
+            sum+=diff[i];
+        }
+
+        return k;
+    }
 
 
 int32_t main() {
@@ -18,11 +41,11 @@ int32_t main() {
     freopen("Error.txt","w",stderr);
    #endif
   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-  int t;
-  cin>>t; 
-  while(t--){
-    solve();
-  }   
+  
+
+  vector<int>v={2,0,2}; 
+  vector<vector<int>> q={{0,2,1},{0,2,1},{1,1,3}};
+  cout<<minZeroArray(v,q);
   return 0;
   
 }
--- cp_with_java/src/main/java/org/example/Main.java
+++ cp_with_java/src/main/java/org/example/Main.java
@@ -1,13 +1,100 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList arrayList=new ArrayList<int>();
-//        Scanner scanner=new Scanner(System.in);
-//        int a=scanner.nextInt();
-//        System.out.println(a);
+
+        // create an array of 5 integer
+        int[] numArray=new int[5];
+        numArray[0]=343;
+        numArray[3]=89;
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print(numArray[i]);
+//            System.out.print(' ');
+//        }
+        /**
+         * Access: O(1)
+         * Search: O(n)
+         * Insertion: O(n)
+         * Deletion: O(n)
+         */
+
+        ArrayList<Integer> arrayList=new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            Random random=new Random();
+            arrayList.add(random.nextInt(600));
+        }
+        //Similar to an array, but can change size
+        //Good for storing and accessing items quickly
+        //Not good for adding or removing items in the middle
+//        for (int x:arrayList){
+//            System.out.print(x);
+//            System.out.print(' ');
+//        }
+//        System.out.println();
+
+        Stack<Integer> stack=new Stack<Integer>();
+        Queue<Integer> queue=new LinkedList<Integer>();
+        for(int i=0;i<5;i++){
+            int x=new Random().nextInt(50);
+            stack.push(x);
+            queue.add(x);
+        }
+        while (!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+        }
+        while (!queue.isEmpty()){
+            System.out.println(queue.peek());
+            queue.remove();
+        }
+        /**
+         * Push: O(1)
+         * Pop: O(1)
+         * Peek: O(1)
+         * Search: O(n)
+         */
+
+
     }
+
+    public int minimumIndex(List<Integer> nums) {
+
+        Map<Integer,Integer> mp=new HashMap<>();
+        int n=nums.size();
+        for (Integer num : nums) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+        Map<Integer,Integer> p=new HashMap<>();
+        for(int i=0;i<n;i++){
+            p.put(nums.get(i), p.getOrDefault(nums.get(i), 0) + 1);
+           mp.put(nums.get(i),mp.get(nums.get(i))-1);
+            if(p.get(nums.get(i))*2>i+1&&mp.get(nums.get(i))*2>n-1-i) return i;
+        }
+        return -1;
+    }
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n=colors.length;
+        int[] v=new int[n+k-1];
+        for (int i = 0; i <n+k-1 ; i++) {
+            v[i]=colors[i%n];
+        }
+        int left=0,ans=0;
+        for (int i = 1; i < n+k; i++) {
+            if(v[i]==v[i-1]){
+                left=i;
+                continue;
+            }
+            if(i-left<k)continue;
+            ans++;
+            left++;
+        }
+        return ans;
+
+
+    }
+
+
 }
